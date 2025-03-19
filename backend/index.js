d0a42f8c49886c3a0fc1b032caef85050cb70a1c
Kumar0510 <149935210+Kumar0510@users.noreply.github.com>
2025-03-19 20:18:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,6 @@ app.use("/webhooks", webHookRouter);
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
   next();
 });
 
